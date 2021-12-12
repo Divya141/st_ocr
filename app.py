603c5c5bac49ac12f4cb86967730de2c9e48d997
Divya141 <96006002+Divya141@users.noreply.github.com>
2021-12-12 20:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import cv2
 import pytesseract
 from PIL import Image #python imaging libraries to open images
 import numpy as np
-pytesseract.pytesseract.tesseract_cmd="/app/.apt/usr/bin/tesseract"
+pytesseract.pytesseract.tesseract_cmd = "/app/.apt/usr/bin/tesseract"
+st.set_option('deprecation.showfileUploaderEncoding',False)
 st.title("Optical character recognition")
 st.text("Upload the image")
-uploaded_file=st.sidebar.file_uploader("Chose an image:",type=["jpg","png","jpeg"])
+uploaded_file = st.sidebar.file_uploader("Chose an image:", type=["jpg","png","jpeg"])
 if uploaded_file is not None:
   img=Image.open(uploaded_file)
+  st.image(img, caption='Uploaded Image'.use_column_width=True)
   st.write("")
   if st.button("PREDICT"):
     st.write("Result:")
-    info=pytesseract.image_to_string(img)
+    info = pytesseract.image_to_string(img)
     st.title(info)
